8e99eeb886e756baaba78a348796496e462457f8
nightbwolf <166263044+nightbwolf@users.noreply.github.com>
2025-04-14 22:22:38 -0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,8 @@
+carros= { 
+    'marca':'toyota',
+    'modelo':'Etios',
+    'ano':2020
+}
+print(carros)
+print(f'O carro de marca
+{carros['marca']} é do modelo {carros['modelo']} e do ano{carros['ano']}')
